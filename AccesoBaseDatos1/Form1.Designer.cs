5384841a2644518f45b4c0facdbe918c0d61c5b1
RogelioITE <al23760346@ite.edu.mx>
2025-03-31 00:05:19 -0700

    Realización de la Base de Datos, tanto con SQLServer como en MySQL

---
+++ AccesoBaseDatos1/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace AccesoBaseDatos1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNoControl = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCarrera = new System.Windows.Forms.TextBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCrearTabla = new System.Windows.Forms.Button();
+            this.btnCrearBD = new System.Windows.Forms.Button();
+            this.chkMySQL = new System.Windows.Forms.CheckBox();
+            this.chkSQLServer = new System.Windows.Forms.CheckBox();
+            this.btnRefrescar = new System.Windows.Forms.Button();
+            this.dgvAlumnos = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlumnos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No Control:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(59, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(62, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Carrera:";
+            // 
+            // txtNoControl
+            // 
+            this.txtNoControl.Location = new System.Drawing.Point(134, 50);
+            this.txtNoControl.Name = "txtNoControl";
+            this.txtNoControl.Size = new System.Drawing.Size(153, 20);
+            this.txtNoControl.TabIndex = 3;
+            this.txtNoControl.TextChanged += new System.EventHandler(this.txtNoControl_TextChanged);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(134, 83);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(364, 20);
+            this.txtNombre.TabIndex = 4;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // txtCarrera
+            // 
+            this.txtCarrera.Location = new System.Drawing.Point(134, 114);
+            this.txtCarrera.Name = "txtCarrera";
+            this.txtCarrera.Size = new System.Drawing.Size(51, 20);
+            this.txtCarrera.TabIndex = 5;
+            this.txtCarrera.TextChanged += new System.EventHandler(this.txtCarrera_TextChanged);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(44, 215);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(109, 49);
+            this.btnInsertar.TabIndex = 6;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(159, 215);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(109, 49);
+            this.btnActualizar.TabIndex = 7;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(274, 215);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(109, 49);
+            this.btnBorrar.TabIndex = 8;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(389, 215);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(109, 49);
+            this.btnBuscar.TabIndex = 9;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCrearTabla);
+            this.groupBox1.Controls.Add(this.btnCrearBD);
+            this.groupBox1.Controls.Add(this.chkMySQL);
+            this.groupBox1.Controls.Add(this.chkSQLServer);
+            this.groupBox1.Location = new System.Drawing.Point(589, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(159, 154);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Base de datos";
+            // 
+            // btnCrearTabla
+            // 
+            this.btnCrearTabla.Location = new System.Drawing.Point(3, 96);
+            this.btnCrearTabla.Name = "btnCrearTabla";
+            this.btnCrearTabla.Size = new System.Drawing.Size(151, 49);
+            this.btnCrearTabla.TabIndex = 3;
+            this.btnCrearTabla.Text = "Crear Tabla";
+            this.btnCrearTabla.UseVisualStyleBackColor = true;
+            this.btnCrearTabla.Click += new System.EventHandler(this.btnCrearTabla_Click);
+            // 
+            // btnCrearBD
+            // 
+            this.btnCrearBD.Location = new System.Drawing.Point(3, 41);
+            this.btnCrearBD.Name = "btnCrearBD";
+            this.btnCrearBD.Size = new System.Drawing.Size(151, 49);
+            this.btnCrearBD.TabIndex = 2;
+            this.btnCrearBD.Text = "Crear BD";
+            this.btnCrearBD.UseVisualStyleBackColor = true;
+            this.btnCrearBD.Click += new System.EventHandler(this.btnCrearBD_Click);
+            // 
+            // chkMySQL
+            // 
+            this.chkMySQL.AutoSize = true;
+            this.chkMySQL.Location = new System.Drawing.Point(90, 18);
+            this.chkMySQL.Name = "chkMySQL";
+            this.chkMySQL.Size = new System.Drawing.Size(61, 17);
+            this.chkMySQL.TabIndex = 1;
+            this.chkMySQL.Text = "MySQL";
+            this.chkMySQL.UseVisualStyleBackColor = true;
+            this.chkMySQL.CheckedChanged += new System.EventHandler(this.chkMySQL_CheckedChanged);
+            // 
+            // chkSQLServer
+            // 
+            this.chkSQLServer.AutoSize = true;
+            this.chkSQLServer.Location = new System.Drawing.Point(6, 18);
+            this.chkSQLServer.Name = "chkSQLServer";
+            this.chkSQLServer.Size = new System.Drawing.Size(78, 17);
+            this.chkSQLServer.TabIndex = 0;
+            this.chkSQLServer.Text = "SQLServer";
+            this.chkSQLServer.UseVisualStyleBackColor = true;
+            this.chkSQLServer.CheckedChanged += new System.EventHandler(this.chkSQLServer_CheckedChanged);
+            // 
+            // btnRefrescar
+            // 
+            this.btnRefrescar.Location = new System.Drawing.Point(589, 215);
+            this.btnRefrescar.Name = "btnRefrescar";
+            this.btnRefrescar.Size = new System.Drawing.Size(154, 49);
+            this.btnRefrescar.TabIndex = 11;
+            this.btnRefrescar.Text = "Refrescar";
+            this.btnRefrescar.UseVisualStyleBackColor = true;
+            this.btnRefrescar.Click += new System.EventHandler(this.btnRefrescar_Click);
+            // 
+            // dgvAlumnos
+            // 
+            this.dgvAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAlumnos.Location = new System.Drawing.Point(12, 288);
+            this.dgvAlumnos.Name = "dgvAlumnos";
+            this.dgvAlumnos.Size = new System.Drawing.Size(776, 150);
+            this.dgvAlumnos.TabIndex = 12;
+            this.dgvAlumnos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAlumnos_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvAlumnos);
+            this.Controls.Add(this.btnRefrescar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.txtCarrera);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtNoControl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Acceso a Base de Datos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlumnos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNoControl;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCarrera;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkMySQL;
+        private System.Windows.Forms.CheckBox chkSQLServer;
+        private System.Windows.Forms.Button btnCrearTabla;
+        private System.Windows.Forms.Button btnCrearBD;
+        private System.Windows.Forms.Button btnRefrescar;
+        private System.Windows.Forms.DataGridView dgvAlumnos;
+    }
+}
+
---
+++ AccesoBaseDatos1/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace AccesoBaseDatos1
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "";  // Se actualizará dinámicamente
+        private SqlConnection sqlConnection;   // Para SQL Server
+        private MySqlConnection mySqlConnection; // Para MySQL
+
+        // Variables de conexión para MySQL
+        private string Servidor = "localhost";
+        private string Basedatos = "ESCOLAR";
+        private string UsuarioId = "root";
+        private string Password = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            CargarDatos();  // Cargar los datos al iniciar el formulario
+        }
+
+        private void chkSQLServer_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkSQLServer.Checked)
+            {
+                chkMySQL.Checked = false;
+                connectionString = "Server=BOREGO-PC\\SQLEXPRESS;Database=ESCOLAR;User Id=sa;Password=roguez64;";
+            }
+        }
+
+        private void txtNoControl_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCarrera_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (chkSQLServer.Checked)
+                {
+                    using (sqlConnection = new SqlConnection(connectionString))
+                    {
+                        sqlConnection.Open();
+                        string query = "INSERT INTO Estudiante VALUES (@NoControl, @Nombre, @Carrera)";
+                        SqlCommand command = new SqlCommand(query, sqlConnection);
+                        command.Parameters.AddWithValue("@NoControl", int.Parse(txtNoControl.Text));
+                        command.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                        command.Parameters.AddWithValue("@Carrera", txtCarrera.Text);
+                        command.ExecuteNonQuery();
+                    }
+                }
+                else if (chkMySQL.Checked)
+                {
+                    using (mySqlConnection = new MySqlConnection(connectionString))
+                    {
+                        mySqlConnection.Open();
+                        string query = "INSERT INTO Estudiante (NoControl, Nombre, Carrera) VALUES (@NoControl, @Nombre, @Carrera)";
+                        MySqlCommand command = new MySqlCommand(query, mySqlConnection);
+                        command.Parameters.AddWithValue("@NoControl", int.Parse(txtNoControl.Text));
+                        command.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                        command.Parameters.AddWithValue("@Carrera", txtCarrera.Text);
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show("Registro insertado correctamente.");
+                CargarDatos();  // Refresca la tabla automáticamente
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (chkSQLServer.Checked)
+                {
+                    using (sqlConnection = new SqlConnection(connectionString))
+                    {
+                        sqlConnection.Open();
+                        string query = "UPDATE Estudiante SET Nombre=@Nombre, Carrera=@Carrera WHERE NoControl=@NoControl";
+                        SqlCommand command = new SqlCommand(query, sqlConnection);
+                        command.Parameters.AddWithValue("@NoControl", int.Parse(txtNoControl.Text));
+                        command.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                        command.Parameters.AddWithValue("@Carrera", txtCarrera.Text);
+                        command.ExecuteNonQuery();
+                    }
+                }
+                else if (chkMySQL.Checked)
+                {
+                    using (mySqlConnection = new MySqlConnection(connectionString))
+                    {
+                        mySqlConnection.Open();
+                        string query = "UPDATE Estudiante SET Nombre=@Nombre, Carrera=@Carrera WHERE NoControl=@NoControl";
+                        MySqlCommand command = new MySqlCommand(query, mySqlConnection);
+                        command.Parameters.AddWithValue("@NoControl", int.Parse(txtNoControl.Text));
+                        command.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                        command.Parameters.AddWithValue("@Carrera", txtCarrera.Text);
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show("Registro actualizado correctamente.");
+                CargarDatos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (chkSQLServer.Checked)
+                {
+                    using (sqlConnection = new SqlConnection(connectionString))
+                    {
+                        sqlConnection.Open();
+                        string query = "DELETE FROM Estudiante WHERE NoControl=@NoControl";
+                        SqlCommand command = new SqlCommand(query, sqlConnection);
+                        command.Parameters.AddWithValue("@NoControl", int.Parse(txtNoControl.Text));
+                        command.ExecuteNonQuery();
+                    }
+                }
+                else if (chkMySQL.Checked)
+                {
+                    using (mySqlConnection = new MySqlConnection(connectionString))
+                    {
+                        mySqlConnection.Open();
+                        string query = "DELETE FROM Estudiante WHERE NoControl=@NoControl";
+                        MySqlCommand command = new MySqlCommand(query, mySqlConnection);
+                        command.Parameters.AddWithValue("@NoControl", int.Parse(txtNoControl.Text));
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show("Registro eliminado correctamente.");
+                CargarDatos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (chkSQLServer.Checked)
+                {
+                    using (sqlConnection = new SqlConnection(connectionString))
+                    {
+                        sqlConnection.Open();
+                        string query = "SELECT * FROM Estudiante WHERE NoControl=@NoControl";
+                        SqlCommand command = new SqlCommand(query, sqlConnection);
+                        command.Parameters.AddWithValue("@NoControl", txtNoControl.Text);
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            txtNombre.Text = reader["Nombre"].ToString();
+                            txtCarrera.Text = reader["Carrera"].ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Registro no encontrado.");
+                        }
+                        reader.Close();
+                    }
+                }
+                else if (chkMySQL.Checked)
+                {
+                    using (mySqlConnection = new MySqlConnection(connectionString))
+                    {
+                        mySqlConnection.Open();
+                        string query = "SELECT * FROM Estudiante WHERE NoControl=@NoControl";
+                        MySqlCommand command = new MySqlCommand(query, mySqlConnection);
+                        command.Parameters.AddWithValue("@NoControl", txtNoControl.Text);
+                        MySqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            txtNombre.Text = reader["Nombre"].ToString();
+                            txtCarrera.Text = reader["Carrera"].ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Registro no encontrado.");
+                        }
+                        reader.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void chkMySQL_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkMySQL.Checked)
+            {
+                chkSQLServer.Checked = false;
+                connectionString = $"Server={Servidor};Database={Basedatos};User Id={UsuarioId};Password={Password};";
+            }
+        }
+
+        private void btnCrearBD_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (chkSQLServer.Checked)
+                {
+                    using (sqlConnection = new SqlConnection(connectionString))
+                    {
+                        sqlConnection.Open();
+                        string query = "CREATE DATABASE ESCOLAR";
+                        SqlCommand command = new SqlCommand(query, sqlConnection);
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Base de datos creada correctamente en SQL Server.");
+                    }
+                }
+                else if (chkMySQL.Checked)
+                {
+                    using (mySqlConnection = new MySqlConnection($"Server={Servidor};User Id={UsuarioId};Password={Password};"))
+                    {
+                        mySqlConnection.Open();
+                        string query = "CREATE DATABASE ESCOLAR";
+                        MySqlCommand command = new MySqlCommand(query, mySqlConnection);
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Base de datos creada correctamente en MySQL.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnCrearTabla_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (chkSQLServer.Checked)
+                {
+                    using (sqlConnection = new SqlConnection(connectionString))
+                    {
+                        sqlConnection.Open();
+                        string query = "USE ESCOLAR; CREATE TABLE Estudiante (NoControl INT PRIMARY KEY, Nombre NVARCHAR(100), Carrera NVARCHAR(50));";
+                        SqlCommand command = new SqlCommand(query, sqlConnection);
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Tabla creada correctamente en SQL Server.");
+                    }
+                }
+                else if (chkMySQL.Checked)
+                {
+                    using (mySqlConnection = new MySqlConnection(connectionString))
+                    {
+                        mySqlConnection.Open();
+                        string query = "CREATE TABLE IF NOT EXISTS Estudiante (NoControl INT PRIMARY KEY, Nombre VARCHAR(100), Carrera VARCHAR(50));";
+                        MySqlCommand command = new MySqlCommand(query, mySqlConnection);
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Tabla creada correctamente en MySQL.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnRefrescar_Click(object sender, EventArgs e)
+        {
+            txtNoControl.Clear();
+            txtNombre.Clear();
+            txtCarrera.Clear();
+            CargarDatos();  // Llama a la función para actualizar el `DataGridView`
+        }
+
+        private void dgvAlumnos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void CargarDatos()
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+
+                if (chkSQLServer.Checked)
+                {
+                    using (sqlConnection = new SqlConnection(connectionString))
+                    {
+                        sqlConnection.Open();
+                        string query = "SELECT * FROM Estudiante";
+                        SqlDataAdapter adapter = new SqlDataAdapter(query, sqlConnection);
+                        adapter.Fill(dt);
+                    }
+                }
+                else if (chkMySQL.Checked)
+                {
+                    using (mySqlConnection = new MySqlConnection(connectionString))
+                    {
+                        mySqlConnection.Open();
+                        string query = "SELECT * FROM Estudiante";
+                        MySqlDataAdapter adapter = new MySqlDataAdapter(query, mySqlConnection);
+                        adapter.Fill(dt);
+                    }
+                }
+
+                dgvAlumnos.DataSource = dt;  // Asignamos los datos al DataGridView
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los datos: " + ex.Message);
+            }
+        }
+    }
+}
